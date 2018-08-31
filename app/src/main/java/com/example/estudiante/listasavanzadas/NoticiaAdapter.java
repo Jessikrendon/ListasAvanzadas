733d0af63677a1ca7a32b673d6fe7cc3c12a6211
Jerry <jlminottar@gmail.com>
2018-08-31 15:28:46 -0500

    Lista de Noticias Generar noticia con fecha actual automática

--- app/src/main/java/com/example/estudiante/listasavanzadas/NoticiaAdapter.java
+++ app/src/main/java/com/example/estudiante/listasavanzadas/NoticiaAdapter.java
@@ -2,11 +2,14 @@ package com.example.estudiante.listasavanzadas;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -43,7 +46,7 @@ public class NoticiaAdapter extends BaseAdapter{
     }
 //Generar un rengón por objeto
     @Override
-    public View getView(int position, View view, ViewGroup viewGroup) {
+    public View getView(final int position, View view, ViewGroup viewGroup) {
 
         //Pasa de xml (txt) a una vista (algo que entiende android
         LayoutInflater inflater= activity.getLayoutInflater();
@@ -54,10 +57,27 @@ public class NoticiaAdapter extends BaseAdapter{
         TextView item_titulo = renglon.findViewById(R.id.item_titulo);
         TextView item_fecha = renglon.findViewById(R.id.item_fecha);
         TextView item_descripcion = renglon.findViewById(R.id.item_descripcion);
+        Button item_action = renglon.findViewById(R.id.item_action);
 
         item_titulo.setText(noticias.get(position).getTitulo());
         item_fecha.setText(noticias.get(position).getFecha());
         item_descripcion.setText(noticias.get(position).getDescripcion());
+        item_action.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // Toast.makeText(activity, "POS"+ position, Toast.LENGTH_SHORT).show();
+               // noticias.remove(position);
+                //notifyDataSetChanged();
+
+                Intent intent = new Intent(activity,NoticiaCompleta.class);
+                activity.startActivity(intent);
+
+
+                //Si fuera desde la MainActivity
+                //Intent i= new Intent(MainActivity.this, NoticiaCompleta.class);
+                //startActivity(i);
+            }
+        });
 
         return renglon;
     }
---
+++ app/src/main/java/com/example/estudiante/listasavanzadas/NoticiaCompleta.java
@@ -0,0 +1,13 @@
+package com.example.estudiante.listasavanzadas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoticiaCompleta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noticia_completa);
+    }
+}
